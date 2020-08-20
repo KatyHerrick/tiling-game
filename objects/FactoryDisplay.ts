9030d9bfcee97c11e4a37dcdc865f48a23f54e51
Katy Herrick <kmh530@nyu.edu>
2020-08-20 18:15:52 -0400

    add FactoryDisplay tests and components

--- objects/FactoryDisplay.ts
+++ objects/FactoryDisplay.ts
@@ -1,3 +1,53 @@
-export class FactoryDisplay {}
+import { Tile } from './GameTiles';
 
-export class TableCenter {}
+class FactoryDisplay {
+	tiles: Array<Tile>;
+
+	constructor() {
+		this.tiles = [];
+	}
+
+	deal(tiles: Array<Tile>) {
+		this.tiles.push(...tiles);
+	}
+
+	chooseTiles(color: string) {
+		const forPlayer = this.tiles.filter(tile => tile.color === color);
+		const forCenter = this.tiles.filter(tile => tile.color !== color);
+		this.tiles = [];
+
+		return [forPlayer, forCenter];
+	}
+
+	availableColors() {
+		return this.tiles.map(tile => tile.color);
+	}
+
+}
+
+class TableCenter extends FactoryDisplay {
+	constructor() {
+		super();
+	}
+
+	deal() {
+		this.tiles.push(new Tile('firstPlayerToken'));
+	}
+
+	add(tiles: Array<Tile>) {
+		this.tiles.push(...tiles);
+	}
+
+	// TODO: Refactor so destructuring output isn't necessary
+	// TypeScript doesn't like the overriding return change from [][] to []
+	chooseTiles(color: string) {
+		const tiles = this.tiles.filter(tile => tile.color === color);
+		if (this.tiles[0].color === 'firstPlayerToken') {
+			tiles.push(this.tiles.shift());
+		}
+		return [tiles];
+	}
+
+}
+
+export { FactoryDisplay, TableCenter };
--- tests/FactoryDisplay.spec.ts
+++ tests/FactoryDisplay.spec.ts
@@ -1,7 +1,76 @@
 import { FactoryDisplay, TableCenter } from '../objects/FactoryDisplay';
+import { Tile } from '../objects/GameTiles';
 
 describe('FactoryDisplay', function() {
-  it('has tests', function() {
-    expect(true).toBe(true);
+  it('is initialized empty', function() {
+		const display = new FactoryDisplay();
+    expect(display.tiles.length).toBe(0);
+  });
+
+  it('can deal tiles to self', function() {
+		const display = new FactoryDisplay();
+		const tilesToDeal = [
+			new Tile('red'),
+			new Tile('red'),
+			new Tile('red'),
+			new Tile('yellow')
+		];
+		display.deal(tilesToDeal)
+    expect(display.tiles.length).toBe(4);
+  });
+
+  it('can choose tiles of a given color', function() {
+		const display = new FactoryDisplay();
+		const tilesToDeal = [
+			new Tile('red'),
+			new Tile('red'),
+			new Tile('red'),
+			new Tile('yellow')
+		];
+		display.deal(tilesToDeal);
+		const [forPlayer, forCenter] = display.chooseTiles('red');
+    expect(forPlayer.length).toBe(3);
+  });
+
+  it('returns unchosen tiles to move to the center', function() {
+		const display = new FactoryDisplay();
+		const tilesToDeal = [
+			new Tile('red'),
+			new Tile('red'),
+			new Tile('red'),
+			new Tile('yellow')
+		];
+		display.deal(tilesToDeal);
+		const [forPlayer, forCenter] = display.chooseTiles('red');
+		expect(forCenter.length).toBe(1);
+  });
+});
+
+describe('TableCenter', function() {
+  it('is initialized empty', function() {
+    const center = new TableCenter();
+    expect(center.tiles.length).toBe(0);
+  });
+
+  it('puts the first player token in upon deal', function() {
+    const center = new TableCenter();
+    center.deal();
+    expect(center.tiles[0].color).toBe('firstPlayerToken');
+  });
+
+  it('can add given tiles to self', function() {
+    const center = new TableCenter();
+    center.deal();
+    center.add([new Tile('red')]);
+    expect(center.tiles.length).toBe(2);
+  });
+
+  it('returns the first player token if it exists', function() {
+    const center = new TableCenter();
+    center.deal();
+    center.add([new Tile('red'), new Tile('blue')]);
+    const [chosenTiles] = center.chooseTiles('red');
+    expect(chosenTiles.length).toBe(2);
+    expect(chosenTiles[1].color).toBe('firstPlayerToken');
   });
 });
