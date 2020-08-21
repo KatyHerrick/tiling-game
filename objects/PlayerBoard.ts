0f2c70a7316a551d80ca7eebe052c766d5593f43
Katy Herrick <kmh530@nyu.edu>
2020-08-21 11:29:30 -0400

    add PlayerBoard tests and component

--- objects/PlayerBoard.ts
+++ objects/PlayerBoard.ts
@@ -1,1 +1,31 @@
-export class PlayerBoard {}
+import { FactoryDisplay, TableCenter } from '../objects/FactoryDisplay';
+import { FloorLine } from './FloorLine';
+import { StagingArea } from './StagingArea';
+import { Tile } from './GameTiles';
+import { Wall } from './Wall';
+
+
+class PlayerBoard {
+	stagingArea: StagingArea;
+	wall: Wall;
+	floor: FloorLine;
+	points: number;
+
+	constructor() {
+		this.stagingArea = new StagingArea();
+		this.wall = new Wall();
+		this.floor = new FloorLine();
+		this.points = 0;
+	}
+
+	takeFromDisplay(display: FactoryDisplay, color: string) {
+		const [forPlayer] = display.chooseTiles(color);
+		return forPlayer;
+	}
+
+	moveToStagingArea(tiles: Array<Tile>, row: number) {
+		this.stagingArea.add(row, tiles, this.wall);
+	}
+}
+
+export { PlayerBoard };
--- tests/PlayerBoard.spec.ts
+++ tests/PlayerBoard.spec.ts
@@ -1,7 +1,48 @@
+import { FactoryDisplay, TableCenter } from '../objects/FactoryDisplay';
 import { PlayerBoard } from '../objects/PlayerBoard';
+import { Tile } from '../objects/GameTiles';
 
+// TODO: Remove setup redundancy
 describe('PlayerBoard', function() {
-  it('has tests', function() {
-    expect(true).toBe(true);
+  it('has all of the proper components', function() {
+    const player = new PlayerBoard();
+    expect(player).toHaveProperty('stagingArea');
+    expect(player).toHaveProperty('wall');
+    expect(player).toHaveProperty('floor');
+    expect(player).toHaveProperty('points');
+  });
+
+  it('initializes with 0 points', function() {
+    const player = new PlayerBoard();
+    expect(player.points).toBe(0);
+  });
+
+  it('can take tiles given a display and color', function() {
+    const player = new PlayerBoard();
+    const display = new FactoryDisplay();
+    const tilesToDeal = [
+      new Tile('red'),
+      new Tile('red'),
+      new Tile('red'),
+      new Tile('yellow')
+    ];
+    display.deal(tilesToDeal);
+    const chosenTiles = player.takeFromDisplay(display, 'red');
+    expect(chosenTiles.length).toBe(3);
+  });
+
+  it('can move tiles from a display to the staging area', function() {
+    const player = new PlayerBoard();
+    const display = new FactoryDisplay();
+    const tilesToDeal = [
+      new Tile('red'),
+      new Tile('red'),
+      new Tile('red'),
+      new Tile('yellow')
+    ];
+    display.deal(tilesToDeal);
+    const chosenTiles = player.takeFromDisplay(display, 'red');
+    player.moveToStagingArea(chosenTiles, 2);
+    expect(player.stagingArea.rows[2].tiles.length).toBe(3);
   });
 });
